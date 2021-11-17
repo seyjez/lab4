3f7c1cda021a299dcbe209707fb2d2db4911c8d2
LastGuardian89 <dmitriy-bobrovskiy03@mai.ru>
2021-11-17 22:15:02 +0300

    Main ver

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,5 +1,10 @@
 #include <header.hpp>
 #include <iostream>
+
+
 int main() {
-  std::cout<<"all system in online!";
+  // path  менять в зависимости от системы
+  std::string path("/home/enigma/twinLabs/c++ developer/lab 4/lab-04-boost-filesystem/misc/ftp");
+  Filesystem test(path);
+  std::cout << test;
 }
